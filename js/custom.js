2d77fa66cb9f198f3ea4f48ef8e0602101d272eb
abiodun603 <abioduncsc@gmail.com>
2021-11-03 02:58:04 +0100

    styling improvement

--- js/custom.js
+++ js/custom.js
@@ -49,3 +49,19 @@ $(document).ready(function() {
       }
     })
   });
+
+var tab_buttons = $('.late__button button');
+var tab_content = $('.tabContent');
+var ArrayButton = Array.from(tab_buttons);
+var ArrayContent = Array.from(tab_content);
+function showpanel (panelIndex){
+        ArrayButton.forEach((button) => {
+                $(button).removeClass('active');     
+        });
+        $(ArrayButton[panelIndex]).addClass('active');
+        ArrayContent.forEach((content) => {
+                $(content).css('display','none');
+        });
+        $(ArrayContent[panelIndex]).css('display','flex');      
+}
+showpanel(0);  
